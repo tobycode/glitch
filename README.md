dfc5e6a8aa4ae9644f947b7ab0e79e10b1c236e6
Toby Jones <toby@tobycode.me>
2015-05-24 19:59:17 +0100

    UI, stability improvements, readme tweaks
    Added a user interface, improved error handling, and updated the readme

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 #glitch.py
 Randomly changes binary file data in order to create glitch art.
 ##Usage
-```./glitch <infile> [percent] [outfile]```
+```./glitch [infile] [percent] [outfile]```
         
 		infile: the file to take as input
 		
         percent: the percentage of the file to corrupt, by default, 0.1%
 		
         outfile: the file to output, by default <filename>-glitched.<fileext>
+        
+        if the script is run with no arguments, you will be prompted for these pieces of information
 
 Play around with different values for percent, 0.1 seems to work okay for most image formats, 0.01 worked okay for avi video.
--- glitch.py
+++ glitch.py
@@ -1,62 +1,118 @@
-#! /usr/bin/env python
-docstring = '''Randomly changes binary file data in order to create glitch art.
-    Usage: glitch <infile> [percentage] [outfile]
-        infile: the file to take as input
-        percent: the percentage of the file to corrupt, by default, 0.1%
-        outfile: the file to output, by default <filename>-glitched.<fileext>
-    '''
+#!/usr/bin/env python
+# glitch.py:
+# Randomly corrupts bytes within a file to create glitch art.
+# Just run the script and use the interface.
+# If you want, you can automate the process with the following command line arguments:
+# glitch.py [input] [amount] [output]
+# Original code written by Paul Sajna (https://github.com/sajattack : https://github.com/sajattack/glitch/)
+# Interface and optimisations by Toby Jones (https://github.com/tobyjonesdev)
+# Use with caution. Written against Python3.
 
 import sys, os, random
+header_length = {'jpg': 9, 'png': 8, 'bmp': 54, 'gif': 14, 'tiff': 8}
 
-headersize = {'jpg': 9, 'png': 8, 'bmp': 54, 'gif': 14, 'tiff': 8}  
+# function for corrupting bytes. written by sajattack
+def glitch(path, amount, output, file_extension):
+	try:
+		# create a few temp file objects for our input and output
+		with open(path, 'rb') as fin:
+			with open(output, 'wb') as fout:
+				# get file extension from path
+				ext = path.split(".")[::-1]
+				try:
+					for byte in range(header_length[file_extension[0]]):
+						bin = fin.read(1)
+						bout = bin
+						fout.write(bout)
+				except KeyError:
+					print("Sorry, this file extension is not supported.")
+					return
+				bytes_written = 0
+				print("Writing bytes", end="")
+				while True:
+					bin = fin.read(1)
+					if not bin:
+						break
+					if (random.random() < amount/100):
+						bout = os.urandom(1)
+					else:
+						bout = bin
+					fout.write(bout)
+					bytes_written += 1
+					if bytes_written % 1000 == 0:
+						print(".", end="")
+				fout.flush()
+				fout.close()
+	except FileNotFoundError:
+		print("Sorry, your input file couldn't be found.")
 
-def glitch(infile, percent, outfile):
-    with open(infile, 'rb') as inf:
-        with open(outfile, 'wb') as outf:
-
-            #copy file header
-            fileext = infile.split(".")[1]
-            try:
-                for byte in range(headersize[fileext]):
-                    inbyte = inf.read(1)
-                    outbyte = inbyte
-                    outf.write(outbyte)
-            except KeyError:
-                pass
-                
-            while True:
-                inbyte = inf.read(1)
-                if not inbyte:
-                    break
-                if (random.random() < percent/100):
-                    outbyte = os.urandom(1)
-                    #outbyte = '\x00' 
-                else:
-                    outbyte = inbyte
-                outf.write(outbyte)
-
-
-if __name__ == "__main__":
-    argc = len(sys.argv)
-    if (argc == 2):
-        infile = sys.argv[1]
-        percent = 0.1
-        filename = infile.split(".")[0]
-        fileext = infile.split(".")[1]
-        outfile = filename + "-glitched" + "." + fileext
-        glitch(infile, percent, outfile)
-    elif (argc == 3):
-        infile = sys.argv[1]
-        percent = float(sys.argv[2])
-        filename = infile.split(".")[0]
-        fileext = infile.split(".")[1]
-        outfile = filename + "-glitched" + "." + fileext
-        glitch(infile, percent, outfile)
-    elif (argc == 4):
-        infile = sys.argv[1]
-        percent = float(sys.argv[2])
-        outfile = sys.argv[3]
-        glitch(infile, percent, outfile)
-    else:
-       print(docstring)
-        
+def invokeUserInterface():
+	path = "null"
+	amount = 0.1
+	output = "null"
+	file_extension = ""
+	if __name__ == "__main__":
+		if len(sys.argv) == 3:
+			path = sys.argv[1]
+			file_extension = path.split(".")[::-1]
+		elif len(sys.argv) == 4:
+			path = sys.argv[1]
+			file_extension = path.split(".")[::-1]
+			try:
+				amount = float(sys.argv[2])
+			except ValueError:
+				print("[Glitch] Bad value entered for amount. Assuming default.")
+				amount = 0.1
+		elif len(sys.argv) == 5:
+			path = sys.argv[1]
+			file_extension = path.split(".")[::-1]
+			try:
+				amount = float(sys.argv[2])
+			except ValueError:
+				print("[Glitch] Bad value entered for amount. Assuming default.")
+				amount = 0.1
+			output = sys.argv[3]
+		elif len(sys.argv) > 5:
+			print("[Glitch] Too many arguments entered. Accepting the first three.")
+			path = sys.argv[1]
+			file_extension = path.split(".")[::-1]
+			try:
+				amount = float(sys.argv[2])
+			except ValueError:
+				print("[Glitch] Bad value entered for amount. Assuming default.")
+				amount = 0.1
+			output = sys.argv[3]
+		print("glitch.py by sajattack, modified by tobyjonesdev\n")
+		while path == "null":
+			path = input("Enter the path to your input file:\n>>> ")
+		
+		while True:
+			try:
+				tmp = input("Enter the percentage of bits to corrupt, or hit enter for 0.1%:\n>>> ")
+				if not tmp:
+					break
+				float(tmp)
+				break
+			except ValueError:
+				pass
+		
+		file_extension = path.split(".")[::-1]
+		while output == "null":
+			tmp = input("Enter the path to your output file, or nothing to generate one:\n>>> ")
+			if not tmp:
+				output = ""
+				for i in path.split(".")[:-1]:
+					output += i
+				output += "-glitched."
+				output += str(file_extension[0])
+			else:
+				output = tmp
+		print("Okay, glitching...")
+		print("Input:\n\t" + path)
+		print("Going to corrupt " + str(amount) + "% of bits.")
+		print("Output:\n\t" + output)
+		glitch(path, amount, output, file_extension)
+	else:
+		print("[Glitch] This function cannot be invoked from within another module. Sorry.")
+		sys.exit(1)
+invokeUserInterface()
